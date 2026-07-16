2787cd1650cdf42e8216d8077855742e2b8b2878
erik <erikferreira03@outlook.com>
2026-07-16 17:17:35 -0300

    first commit

---
+++ src/test/java/com/erikferreira/stocksync/StockSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikferreira.stocksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
